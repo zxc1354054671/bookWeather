daaca8c03f6f18aa86dd0ef1b33fbc467bd06cd3
liuyongjian <1354054671@qq.com>
2020-01-11 15:56:21 +0800

    config

---
+++ app/src/main/java/com/bw/bookweather/util/Utility.java
@@ -0,0 +1,16 @@
+package com.bw.bookweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zxc on 2020/1/11.
+ */
+
+public class Utility {
+    public static void sendRequest(String url,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder().url(url).build();
+        client.newCall(req).enqueue(callback);
+    }
+}
